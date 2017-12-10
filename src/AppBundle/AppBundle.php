e88497645e6319bff43adcb2fa7cf02486dc1c0d
Benjamin Issleib <ben@kampfq.eu>
2017-12-10 20:19:06 +0100

    gplify all the things
    Signed-off-by: Benjamin Issleib <ben@kampfq.eu>

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Copyright (c) Benjamin Issleib 2017.
+ * This file is part of the Podlatch Podcast Publisher.
+ * Podlatch Podcast Publisher is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *  Podlatch Podcast Publisher is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with the Podlatch Podcast Publisher. If not, see http://www.gnu.org/licenses/.
+ */
 
 namespace AppBundle;
 
--- src/Podlatch/PublisherBackendBundle/Controller/AdminController.php
+++ src/Podlatch/PublisherBackendBundle/Controller/AdminController.php
@@ -1,11 +1,18 @@
 <?php
 /**
+ * Copyright (c) Benjamin Issleib 2017.
+ * This file is part of the Podlatch Podcast Publisher.
+ * Podlatch Podcast Publisher is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *  Podlatch Podcast Publisher is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with the Podlatch Podcast Publisher. If not, see http://www.gnu.org/licenses/.
+ */
+/**
  * AdminController.php
  *
  * Made with <3 with PhpStorm
- * @author kampfq
+ * @author Benjamin Issleib
  * @copyright 2017 Benjamin Issleib
- * @license    NO LICENSE AVAILIABLE
+ * @license    GPLv3
  * @see
  * @since      File available since Release
  * @deprecated File deprecated in Release
