af6389aaa5b7eb0fc788392823e071dfa6a4915b
Benjamin Issleib <ben@kampfq.eu>
2017-12-10 23:07:52 +0100

    meh

--- src/Podlatch/PublisherCoreBundle/PublisherCoreBundle.php
+++ src/Podlatch/PublisherCoreBundle/PublisherCoreBundle.php
@@ -5,7 +5,7 @@
  * Podlatch Podcast Publisher is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * Podlatch Podcast Publisher is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License along with the Podlatch Podcast Publisher. If not, see http://www.gnu.org/licenses/.
- */ 
+ */
 namespace Podlatch\PublisherCoreBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
